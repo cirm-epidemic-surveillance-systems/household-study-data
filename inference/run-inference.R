bf5dff9c4f8dc0e2a2c6e9d55a5ea60260e7b5ac
camacesco <francesco.camaglia@pasteur.fr>
2025-05-01 18:01:19 +0200

    Updated inference.

--- inference/run-inference.R
+++ inference/run-inference.R
@@ -1,44 +1,77 @@
+
 library(tidyverse)
 
-options(scipen = 100)
+# dummy dataset to play around
+proba_asym <- 0.1
+dataset <- read_csv("data/dummy-output.csv") |>
+  mutate(
+    onset_date = floor(onset_date + (onset_date %% 1)),
+    status = if_else(status == 1 & runif(n()) < !!proba_asym, 2L, status),
+    first_pos_date = if_else(status == 2, infec_date + runif(1, min = 2, max = 10), onset_date)
+  ) |>
+  select(-onset_date, -incub_period, -infec_date)
+dataset
 
 model_choice <- "inference/inference-model"
 hhmodel <- housepi::HouseholdModel$new(model_choice)
 
 hhmodel$load_param_list(
-  alpha = 0.1,
-  beta = 0.2,
-  gamma = 1,
+  alpha = list(
+    prior = "unif", args = list(min = 0, max = 1),
+    rule = "lnorm", scale = 1
+  ),
+  beta = list(
+    prior = "unif", args = list(min = 0, max = 1),
+    rule = "lnorm", scale = 1
+  ),
+  gamma = list(
+    prior = "unif", args = list(min = 0, max = 4),
+    rule = "norm", scale = 1
+  ),
   size_ref = 4,
-  ctct_child2child = 1.5,
-  ctct_child2adult = 1.2,
-  ctct_child2senior = 0.5,
-  ctct_adult2child = 1.2,
-  ctct_adult2senior = 0.75,
-  ctct_senior2child = 0.5,
-  ctct_senior2adult = 0.75,
-  ctct_senior2senior = 0.75
+  study_period = 45 # FIXME: study period according to protocol
 )
 
 hhmodel$load_augm_list(
   # first_pos_date of Symptomatic : onset date
   # first_pos_date of Asymptomatic : earliest positive test
   # note: no augmentation, read it from data
-  first_pos_date = "first_pos_date@D",
+  first_pos_date = list(
+    value = "first_pos_date@D",
+    discrete = TRUE
+  ),
   # delay_period of Symptomatic : incubation period
   delay_period = list(
-    model = "reparam_gamma",
+    model = "reparam_gamma", discrete = TRUE,
     init_rand = TRUE,
     args = c(mean = 3, var = 0.75), # FIXME: args according to independent study
-    status = 1
+    status = 1, rule = "draw"
   ),
   # delay_period of Asymptomatic : no a prior
   delay_period = list(
-    model = "unif",
+    model = "unif", discrete = TRUE,
     init_rand = TRUE,
     args = c(min = 0, max = 3), # FIXME: max according to protocol
-    status = 2
+    status = 2, rule = "draw"
   )
 )
 
-simulated <- hhmodel$infer()
+hhmodel$dataset <- dataset |>
+  mutate(age_group = ifelse(age <= 12, "child", "adult"))
+
+# associate group rates
+hhmodel$load_group_rate_list(
+  community = list(by = "age_group"),
+  infectivity = list(by = "age_group"),
+  susceptibility = list(by = "age_group")
+)
+hhmodel$group_rate_tables
+
+# perform inference
+hhmodel$infer(n_chains = 3, n_adapt = 2000, n_steps = 10000,
+              n_burnin = 1000, thin = 10, seed = 3731, progress = 30)
+# show mcmc traces
+bayesplot::mcmc_trace(hhmodel$traces)
+
+# extract results
+hhmodel$traces_results
